d6a0e96e9ccbecf38a1ec89b4d3cc029b3b544b1
user <adruts@mail.ru>
2022-09-21 11:57:30 +0300

    First commit

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-db = SQLAlchemy
+db = SQLAlchemy(app)
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
+    text = db.Column(db.Text, nullable=False)
 
 @app.route('/')
 def index():
