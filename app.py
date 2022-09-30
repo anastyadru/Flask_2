30bbe868addecf537a19c82cddcd5b4607bd9e07
user <adruts@mail.ru>
2022-09-30 21:54:39 +0300

    First commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class Item(db.Model):
 
 @app.route('/')
 def index():
-    items = item.query.order_by(Item.price).
+    items = item.query.order_by(Item.price).all()
     return render_template('index.html')
 
 @app.route('/about')
